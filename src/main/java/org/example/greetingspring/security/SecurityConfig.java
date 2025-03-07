45a684a5ec98c58b78001bf0e26bce3aff9b909b
Chetan07123 <144405476+Chetan07123@users.noreply.github.com>
2025-03-07 21:43:10 +0530

    token Generation

--- src/main/java/org/example/greetingspring/security/SecurityConfig.java
+++ src/main/java/org/example/greetingspring/security/SecurityConfig.java
@@ -1,15 +1,18 @@
-package org.example.greetingspring;
+package org.example.greetingspring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @Configuration
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
